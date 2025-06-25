6e519aab9f09b7b49a00e78fcd39be4082f33941
tran-h <43885150+tran-h@users.noreply.github.com>
2025-06-24 22:37:25 -0400

    feat: add character cards from API

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,50 +1,55 @@
+import { useEffect, useState } from "react";
 import "../styles/Cards.css";
 
+//Top 10 most popular Pokemon in 2021 according to https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_of_the_Year
+const urls = [
+  "https://pokeapi.co/api/v2/pokemon/dedenne",
+  "https://pokeapi.co/api/v2/pokemon/cinccino",
+  "https://pokeapi.co/api/v2/pokemon/sableye",
+  "https://pokeapi.co/api/v2/pokemon/snivy",
+  "https://pokeapi.co/api/v2/pokemon/magnemite",
+  "https://pokeapi.co/api/v2/pokemon/swadloon",
+  "https://pokeapi.co/api/v2/pokemon/pikachu",
+  "https://pokeapi.co/api/v2/pokemon/buzzwole",
+  "https://pokeapi.co/api/v2/pokemon/oshawott",
+  "https://pokeapi.co/api/v2/pokemon/flygon",
+];
+
 export default function Cards() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      try {
+        const responses = await Promise.all(urls.map((url) => fetch(url)));
+
+        const jsonData = await Promise.all(
+          responses.map((res) => {
+            if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+            return res.json();
+          })
+        );
+        setPokemonList(jsonData);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchAll();
+  }, []);
+
   return (
-    <>
-      <div className="container">
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
+    <div className="container">
+      {pokemonList.map((pokemon) => (
+        <div key={pokemon.id} className="card">
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+            alt={pokemon.name}
+          />
+          <div>
+            {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+          </div>
         </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-        <div className="card">
-          <img src="658.png" />
-          <div>Pokemon</div>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
